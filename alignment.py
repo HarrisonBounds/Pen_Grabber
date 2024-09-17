ede32b1184b69d45f1ff61f0b71a2c2f8dd783b8
Harrison Bounds <harrison.bounds777@gmail.com>
2024-09-17 16:31:34 -0500

    masked image to detect purple

--- alignment.py
+++ alignment.py
@@ -3,7 +3,8 @@ import numpy as np
 import cv2
 
 class Alignment:
-    def __init__(self):
+    def __init__(self, fn, record, playback):
+        
         self.pipeline = rs.pipeline()
         self.config = rs.config()
         
@@ -14,6 +15,15 @@ class Alignment:
         self.device = self.pipeline_profile.get_device()
         self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
         self.clipping_distance_meters = 1 #In meters
+        
+        filename = f'recordings/{fn}'
+        
+        if record:
+            print("Here")
+            self.config.enable_record_to_file(filename)
+        if playback:
+            self.config.enable_device_from_file(filename)
+    
         
     def getRGB(self):
         found_rgb = False
@@ -48,27 +58,54 @@ class Alignment:
         align = rs.align(align_to)
         return align
     
-    def stream(self, align, clipping_distance):
-        # Streaming loop
-        try:
-            while True:
-                # Get frameset of color and depth
-                frames = self.pipeline.wait_for_frames()
-                # frames.get_depth_frame() is a 640x360 depth image
+    
+    def convert_to_HSV(self, frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        
+        #define range for purple color in HSV
+        lower_purple = np.array([100, 30, 30])
+        upper_purple = np.array([158, 255, 255])
+        
+        mask = cv2.inRange(hsv, lower_purple, upper_purple)
+        res = cv2.bitwise_and(frame, frame, mask=mask)
+        
+        return res
+    
+    #Use loop
+    def stream(self, align, clipping_distance, HSV):
+        while True:
+            # Get frameset of color and depth
+            frames = self.pipeline.wait_for_frames()
+            
+            # frames.get_depth_frame() is a 640x360 depth image
+            # Align the depth frame to color frame
+            aligned_frames = align.process(frames)
 
-                # Align the depth frame to color frame
-                aligned_frames = align.process(frames)
+            # Get aligned frames
+            aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+            color_frame = aligned_frames.get_color_frame()
 
-                # Get aligned frames
-                aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
-                color_frame = aligned_frames.get_color_frame()
-
-                # Validate that both frames are valid
-                if not aligned_depth_frame or not color_frame:
-                    continue
-
+            # Validate that both frames are valid
+            if not aligned_depth_frame or not color_frame:
+                continue
+            
+            color_image = np.asanyarray(color_frame.get_data())
+            depth_image = np.asanyarray(aligned_depth_frame.get_data())
+            
+            if HSV:
+                
+                
+                # Remove background - Set pixels further than clipping_distance to grey
+                grey_color = 153
+                depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+                bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+                res = self.convert_to_HSV(bg_removed)
+                
+                cv2.imshow('Purple Objects', res)
+                
+            else:
                 depth_image = np.asanyarray(aligned_depth_frame.get_data())
-                color_image = np.asanyarray(color_frame.get_data())
+              
 
                 # Remove background - Set pixels further than clipping_distance to grey
                 grey_color = 153
@@ -80,21 +117,16 @@ class Alignment:
                 #   depth on right
                 depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
                 images = np.hstack((bg_removed, depth_colormap))
+                
 
                 cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
                 cv2.imshow('Align Example', images)
-                key = cv2.waitKey(1)
-                # Press esc or 'q' to close the image window
-                if key & 0xFF == ord('q') or key == 27:
-                    cv2.destroyAllWindows()
-                    break
-        finally:
-            self.pipeline.stop()
+            key = cv2.waitKey(1)
+        
+            # Press esc or 'q' to close the image window
+            if key & 0xFF == ord('q') or key == 27:
+                cv2.destroyAllWindows()
+                break
 
-#MAIN
-alignment = Alignment()
-alignment.getRGB()
-ds = alignment.getDepth()
-cd = alignment.clip(ds)
-align = alignment.align()
-alignment.stream(align, cd)
+    def cleanup(self):
+        self.pipeline.stop()
